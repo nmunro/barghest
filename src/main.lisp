b2fd06ea64c8f0c9132700f09f9b5b355c888b58
Neil Munro <neilmunro@gmail.com>
2020-04-15 00:52:48 +0100

    Some code cleanups
    Some code cleanups

--- src/main.lisp
+++ src/main.lisp
@@ -1,22 +1,21 @@
 (defpackage barghest
-  (:use :cl
-        :barghest.request)
+  (:use :cl)
+  (:import-from :barghest.datetime     :datetime)
+  (:import-from :barghest.status-codes :make-status-code)
+  (:import-from :barghest.response     :render)
   (:export #:serve
            #:hello-world))
 (in-package :barghest)
 
-(defparameter *address* "127.0.0.1")
-(defparameter *port* 8080)
-
-(defun serve (app)
-  (usocket:with-server-socket (server-socket (usocket:socket-listen *address* *port*))
+(defun serve (app &key (address "127.0.0.1") (port 8080))
+  (usocket:with-server-socket (server-socket (usocket:socket-listen address port))
     (format t "Starting server on: ~A:~A~%" *address* *port*)
     (unwind-protect
       ; protect form
       (loop (usocket:with-connected-socket (server-connection (usocket:socket-accept server-socket))
         (with-open-stream (stream (usocket:socket-stream server-connection))
           ; Request and response objects are created here
-          (let ((req (barghest.request:make-request stream))
+          (let ((req (barghest.request:make-request   stream))
                 (res (barghest.response:make-response stream)))
               (funcall app req res)))))
 
@@ -25,15 +24,15 @@
       (usocket:socket-close server-socket))))
 
 (defun hello-world (req res)
-  (format t "Hello-World: ~A -> ~A~%" (barghest.datetime:datetime) req)
+  (format t "Hello-World: ~A -> ~A~%" (datetime) req)
 
   (if (equal (barghest.request:path req) "greeting")
     (if (not (gethash "name" (barghest.request:args req)))
-      (barghest.response:send-response res "<html><form>What is your name?<input name='name' /><form></html>")
-      (barghest.response:send-response res (format nil "<html>Nice to meet you, ~A!</html>" (gethash "NAME" (barghest.request:args req)))))
+      (render res "<html><form>What is your name?<input name='name' /><form></html>")
+      (render res (format nil "<html>Nice to meet you, ~A!</html>" (gethash "name" (barghest.request:args req)))))
 
-    (error-404 req res)))
+    (request-abort :404 req res)))
 
-(defun error-404 (req res)
-  (setf (barghest.response:status-code res) (barghest.status-codes:make-status-code :404))
-  (barghest.response:send-response res (format nil "Sorry... I don't know the page: \"~A\"" (barghest.request:path req))))
+(defun request-abort (status req res)
+  (setf (barghest.response:status res) (make-status-code status))
+  (render res (format nil "Sorry... I don't know the page: \"~A\"" (barghest.request:path req))))
--- src/response.lisp
+++ src/response.lisp
@@ -1,44 +1,41 @@
 (defpackage barghest.response
   (:use :cl)
   (:export #:response
-           #:status-code
+           #:status
            #:header
-           #:content
-           #:send-response
+           #:body
+           #:render
            #:make-response))
 (in-package :barghest.response)
 
 (defclass response ()
-  ((status-code :initarg :status-code :initform (barghest.status-codes:make-status-code :200) :accessor status-code)
+  ((status      :initarg :status      :initform (barghest.status-codes:make-status-code :200) :accessor status)
    (header      :initarg :header      :initform (make-hash-table)                             :accessor header)
-   (content     :initarg :content     :initform ""                                            :accessor content)))
+   (body        :initarg :body        :initform ""                                            :accessor body)))
 
-(defgeneric status-code (obj)
+(defgeneric status (obj)
   (:documentation "Gets the status-code of the response object"))
 
 (defgeneric header (obj)
   (:documentation "Gets the header of the response object"))
 
-(defgeneric content (obj)
-  (:documentation "Gets the content of the response object"))
-
-(defgeneric send-response (response data)
-  (:documentation "This takes some data and writes it to the stream"))
+(defgeneric body (obj)
+  (:documentation "Gets the body of the response object"))
 
 (defmethod print-object ((response response) stream)
   (print-unreadable-object (response stream :type t)
     (format stream "~A: /~A" (action response) (path response))))
 
-(defmethod send-response ((response response) data)
-  (format (content response) (format-response (status-code response) data)))
+(defun build-status-line (status)
+  (format nil "HTTP/1.1 ~A ~A" (barghest.status-codes:code status) (barghest.status-codes:description status)))
 
-(defun make-response (stream)
-  (make-instance 'response :content stream))
-
-(defun format-response (status content)
+(defun render (response content)
   (let ((content-length (length content)))
-    (format nil
+    (format (body response)
             "~A~%Content-Type: text/html~%Content-Length: ~A~%~%~A"
-            (format nil "HTTP/1.1 ~A ~A" (barghest.status-codes:code status) (barghest.status-codes:description status))
+            (build-status-line (status response))
             content-length
             content)))
+
+(defun make-response (stream)
+  (make-instance 'response :body stream))
