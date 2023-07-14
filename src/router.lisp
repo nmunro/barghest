191629ff19e57f95263b9609963c224072ccbad0
NMunro <neilmunro@gmail.com>
2023-07-14 01:12:09 +0100

    Added ability to mount collections of urls

--- src/router.lisp
+++ src/router.lisp
@@ -1,8 +1,23 @@
 (defpackage barghest/routes
   (:use :cl)
-  (:export #:defroute))
+  (:export #:+urls+
+           #:defroute
+           #:mount
+           #:path))
 
 (in-package barghest/routes)
 
+(defparameter +urls+ '())
+
 (defun defroute (app route fn &key (method :GET))
   (setf (ningle:route app route :method method) fn))
+
+(defun path (url callback &key (method :GET) (name ""))
+  (list :url url :view callback :method method :name name))
+
+(defun mount (app prefix urls)
+  (dolist (url urls)
+    (let ((route (defroute app (format nil "~A~A" prefix (getf url :url)) (getf url :view) :method (getf url :method))))
+      (when (string/= (getf url :name) "")
+        (push (getf url :url) +urls+)
+        (push (getf url :name) +urls+)))))
